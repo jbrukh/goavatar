b1abcc11d3b9c8312ba62d4e93859379b1f1dc12
Jake Brukhman <brukhman@gmail.com>
2013-04-25 01:15:25 -0400

    Finished server.

--- server/server.go
+++ server/server.go
@@ -1,25 +1,44 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	. "github.com/jbrukh/goavatar"
 	"github.com/jbrukh/goavatar/socket"
+	"log"
 	"net/http"
 )
 
 const (
-	DefaultPort        = "/dev/tty.AvatarEEG03009-SPPDev"
-	DefaultWindowSize  = 10000
-	DefaultRefreshRate = 5
-	DefaultMaxFrames   = 10000
-	WindowMultiple     = 10
+	DefaultSerialPort = "/dev/tty.AvatarEEG03009-SPPDev"
+	DefaultListenPort = 8000
 )
 
 var (
-	serialPort  *string = flag.String("port", DefaultPort, "the serial port for the device")
-	refreshRate *int    = flag.Int("refreshRate", DefaultRefreshRate, "the number of data points to buffer before refreshing")
-	maxFrames   *int    = flag.Int("maxFrames", DefaultMaxFrames, "maximum frames to read before turning off")
-	mockDevice  *bool   = flag.Bool("mockDevice", false, "whether to use the mock device")
+	serialPort *string = flag.String("serialPort", DefaultPort, "the serial port for the device")
+	mockDevice *bool   = flag.Bool("mockDevice", false, "whether to use the mock device")
+	listenPort int     = flag.Bool("listenPort", DefaultListenPort, "the websocket port on which to listen")
 )
 
 func main() {
+	flag.Parse()
+	var device Device
 
+	// get the device
+	if *mockDevice {
+		log.Printf("using the MockDevice")
+		device = NewMockDevice()
+	} else {
+		log.Printf("using the AvatarEEG on port %s", *serialPort)
+		device = NewAvatarDevice(*serialPort)
+	}
+
+	log.Printf("starting server on port %d", *listenPort)
+	port := fmt.Sprintf(":%d", *listenPort)
+	h := socket.Handler(device)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
 }
