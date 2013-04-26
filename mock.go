7ea51e2ea3062533af025686a25bc344aca0eb18
Jake Brukhman <brukhman@gmail.com>
2013-04-26 11:38:41 -0400

    Migrated mock device to use base device.

--- mock.go
+++ mock.go
@@ -1,10 +1,6 @@
 package goavatar
 
 import (
-	"fmt"
-	//"log"
-	//"math/rand"
-	"sync"
 	"time"
 )
 
@@ -13,89 +9,44 @@ import (
 // ----------------------------------------------------------------- //
 
 type MockDevice struct {
-	offSignal chan bool       // send a value to disconnect the device
-	output    chan *DataFrame // output channel
-	connected bool
-	lock      sync.Mutex
+	baseDevice
 }
 
 // NewDevice creates a new Device. The user can then start
 // streaming data by calling Connect() and reading the 
 // output channel.
 func NewMockDevice() *MockDevice {
-	return &MockDevice{
-		offSignal: make(chan bool),
-		connected: false,
-	}
-}
 
-func (d *MockDevice) Connected() bool {
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	return d.connected
-}
-
-func (d *MockDevice) Connect() (output <-chan *DataFrame, err error) {
-	d.lock.Lock()
-	defer d.lock.Unlock()
-
-	// already connected?
-	if d.connected {
-		return nil, fmt.Errorf("Device is already connected.")
-	}
-	d.output = make(chan *DataFrame, DataBufferSize)
-
-	// simulate startup time
-	time.Sleep(time.Second * 1)
-
-	go func() {
-		mockConnection(d.offSignal, d.output)
-	}()
-	d.connected = true
-	return d.output, nil
-}
-
-// Disconnect from the device.
-func (d *MockDevice) Disconnect() (err error) {
-	d.lock.Lock()
-	defer d.lock.Unlock()
-
-	if !d.connected {
-		return
+	// CONNECT
+	connFunc := func() error {
+		// simulate startup time
+		time.Sleep(time.Second * 1)
+		return nil
 	}
 
-	// send the off signal; will block until the
-	// offSignal is processed on the output thread
-	d.offSignal <- true
-	close(d.output)
-	d.connected = false
-	return
-}
+	// DISCONNECT
+	disconnFunc := func() error {
+		return nil // do nothing
+	}
 
-func (d *MockDevice) Out() <-chan *DataFrame {
-	d.lock.Lock()
-	defer d.lock.Unlock()
-	return d.output
-}
-
-func mockConnection(offSignal <-chan bool, output chan<- *DataFrame) {
-	tick := 0
-	for {
-		// break the loop if 
-		// there is an off signal
-		if shouldBreak(offSignal) {
-			break
+	// STREAM
+	streamFunc := func(offSignal <-chan bool, output chan<- *DataFrame) {
+		tick := 0
+		for {
+			if shouldBreak(offSignal) {
+				break
+			}
+			output <- frames[tick%len(frames)]
+			tick++
+			time.Sleep(time.Millisecond * 64) // 15.625 fps == 1 frame every 64 milliseconds
 		}
-		output <- frames[tick%len(frames)]
-		tick++
-		time.Sleep(time.Millisecond * 64) // 15.625 fps == 1 frame every 64 milliseconds
 	}
-}
 
-func (d *MockDevice) Record(file string) (err error) {
-	return
-}
-
-func (d *MockDevice) Stop() {
-
+	return &MockDevice{
+		baseDevice: *newBaseDevice(
+			connFunc,
+			disconnFunc,
+			streamFunc,
+		),
+	}
 }
