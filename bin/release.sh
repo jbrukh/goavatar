c9f266ab69d4f4d1f2ed199c9fcc90cf55f3564e
Jake Brukhman <brukhman@gmail.com>
2013-05-16 20:50:11 -0400

    Added go vet to release script.

--- bin/release.sh
+++ bin/release.sh
@@ -2,3 +2,8 @@ cd $GOPATH/src/github.com/jbrukh/goavatar
 
 export GORACE="log_path=stdout"
 go build -v ./... && go install -v ./... && go test ./...
+
+echo ""
+echo "Suggestions from go-vet..."
+echo ""
+find . -type d | xargs go tool vet --all
