3da3d9e490e19d86e339273e4e50580896e65011
Jake Brukhman <brukhman@gmail.com>
2013-05-19 21:36:34 -0400

    Moved control to separate file.

--- device.go
+++ device.go
@@ -92,59 +92,6 @@ type DeviceImpl interface {
 }
 
 // ----------------------------------------------------------------- //
-// Device Control -- used by implementation providers to report
-// data and know when to disengage
-// ----------------------------------------------------------------- //
-
-// Control is a control structure used by client workers
-// that stream data.
-type Control struct {
-	done chan bool
-	out  chan DataFrame
-	info chan *DeviceInfo
-	d    *BaseDevice
-}
-
-// Create a new Control.
-func newControl(d *BaseDevice) *Control {
-	return &Control{
-		done: make(chan bool),
-		out:  make(chan DataFrame, DataFrameBufferSize),
-		info: make(chan *DeviceInfo, 1),
-		d:    d,
-	}
-}
-
-// ShouldTerminate returns true if and only if the
-// Device is calling for streaming operations to stop.
-func (control *Control) ShouldTerminate() bool {
-	select {
-	case <-control.done:
-		return true
-	default:
-	}
-	return false
-}
-
-// The client worker should send data frames to the
-// Device by calling this method.
-func (control *Control) Send(df DataFrame) {
-	control.out <- df
-}
-
-// The client must send DeviceInfo before sending
-// data.
-func (control *Control) SendInfo(info *DeviceInfo) {
-	control.info <- info
-}
-
-// The client worker should call this method before
-// exiting.
-func (control *Control) Close() {
-	close(control.out)
-}
-
-// ----------------------------------------------------------------- //
 // Device Info -- basic info about the device that should
 // be ascertained on every connect.
 // ----------------------------------------------------------------- //
---
+++ device_control.go
@@ -0,0 +1,54 @@
+package goavatar
+
+// ----------------------------------------------------------------- //
+// Device Control -- used by implementation providers to report
+// data and know when to disengage
+// ----------------------------------------------------------------- //
+
+// Control is a control structure used by client workers
+// that stream data.
+type Control struct {
+	done chan bool
+	out  chan DataFrame
+	info chan *DeviceInfo
+	d    *BaseDevice
+}
+
+// Create a new Control.
+func newControl(d *BaseDevice) *Control {
+	return &Control{
+		done: make(chan bool),
+		out:  make(chan DataFrame, DataFrameBufferSize),
+		info: make(chan *DeviceInfo, 1),
+		d:    d,
+	}
+}
+
+// ShouldTerminate returns true if and only if the
+// Device is calling for streaming operations to stop.
+func (c *Control) ShouldTerminate() bool {
+	select {
+	case <-c.done:
+		return true
+	default:
+	}
+	return false
+}
+
+// The client worker should send data frames to the
+// Device by calling this method.
+func (c *Control) Send(df DataFrame) {
+	c.out <- df
+}
+
+// The client must send DeviceInfo before sending
+// data.
+func (c *Control) SendInfo(info *DeviceInfo) {
+	c.info <- info
+}
+
+// The client worker should call this method before
+// exiting.
+func (c *Control) Close() {
+	close(c.out)
+}
