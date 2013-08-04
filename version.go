761340a1f48620fd42180e54ba2ce0e03adbf1d9
Jake Brukhman <brukhman@gmail.com>
2013-08-04 15:47:36 -0400

    Updating latest Octopus version to b13ccde02340b74bc35f506441fd7a01a0b7d60a.

--- version.go
+++ version.go
@@ -10,7 +10,7 @@ import (
 const (
 	GoavatarVersionMajor = 0
 	GoavatarVersionMinor = 1
-	GoavatarVersionSha   = "c722471e3344b3bd31e5ae3c7fdcf509fed79c5e"
+	GoavatarVersionSha   = "b13ccde02340b74bc35f506441fd7a01a0b7d60a"
 )
 
 func Version() string {
