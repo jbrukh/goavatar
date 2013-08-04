414c6701fe534710256c9988329b2da9113911d9
Jake Brukhman <brukhman@gmail.com>
2013-08-04 15:26:14 -0400

    Updated release for version.

--- bin/release.sh
+++ bin/release.sh
@@ -3,6 +3,19 @@ cd $GOPATH/src/github.com/jbrukh/goavatar
 export GORACE="log_path=stdout"
 go build ./... && go install -v ./... && go test ./...
 
+if [ $? -eq 0 ]; then
+  if [[ -n $(git status -s) ]]; then
+    echo ""
+    echo "WARNING: not updating version, your working tree is dirty."
+    echo ""
+  else
+    SHA=$(git rev-parse HEAD)
+    echo "Updating version to: $SHA"
+    sed -i '' "s#\(GoavatarVersionSha   = \"\)\([a-f0-9]*\)\"#\1$SHA\"#g" version.go
+    git commit -a -m "Updating latest Octopus version to $SHA."
+  fi    
+fi
+
 echo ""
 echo "Suggestions from go-vet..."
 echo ""
