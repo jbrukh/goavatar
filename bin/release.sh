1879f6754026d99ceca6a5e36b4d3524ae64789b
Jake Brukhman <brukhman@gmail.com>
2013-08-04 16:01:05 -0400

    Release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,22 +1,25 @@
 cd $GOPATH/src/github.com/jbrukh/goavatar
 
 export GORACE="log_path=stdout"
-
-if [ "$1" != "--no-version-stamp" ]; then
-  if [[ -n $(git status -s) ]]; then
-    echo ""
-    echo "WARNING: not updating version, your working tree is dirty."
-    echo ""
-  else
-    SHA=$(git rev-parse HEAD)
-    echo ""
-    echo "Updating version to: $SHA"
-    echo ""
-    sed -i '' "s#\(GoavatarVersionSha   = \"\)\([a-f0-9]*\)\"#\1$SHA\"#g" version.go
-  fi
+UPDATED=false
+if [[ -n $(git status -s) ]]; then
+  echo ""
+  echo "WARNING: not updating version, your working tree is dirty."
+  echo ""
+else
+  SHA=$(git rev-parse HEAD)
+  echo ""
+  echo "Updating version to: $SHA"
+  echo ""
+  sed -i '' "s#\(GoavatarVersionSha   = \"\)\([a-f0-9]*\)\"#\1$SHA\"#g" version.go
+  UPDATED=true
 fi
 
 go build ./... && go install -v ./... && go test ./...
+
+if [ $UPDATED ]; then
+    git checkout HEAD version.go
+fi
 
 echo ""
 echo "Suggestions from go-vet..."
