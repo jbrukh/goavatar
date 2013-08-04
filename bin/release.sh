3972f0a3c41f4a73e14a866d8fcd49e90de712a9
Jake Brukhman <brukhman@gmail.com>
2013-08-04 15:56:45 -0400

    Release now no longer does a commit.

--- bin/release.sh
+++ bin/release.sh
@@ -1,20 +1,22 @@
 cd $GOPATH/src/github.com/jbrukh/goavatar
 
 export GORACE="log_path=stdout"
-go build ./... && go install -v ./... && go test ./...
 
-if [ $? -eq 0 ] && [ "$1" != "--no-version-stamp" ]; then
+if [ "$1" != "--no-version-stamp" ]; then
   if [[ -n $(git status -s) ]]; then
     echo ""
     echo "WARNING: not updating version, your working tree is dirty."
     echo ""
   else
     SHA=$(git rev-parse HEAD)
+    echo ""
     echo "Updating version to: $SHA"
+    echo ""
     sed -i '' "s#\(GoavatarVersionSha   = \"\)\([a-f0-9]*\)\"#\1$SHA\"#g" version.go
-    git commit -a -m "Updating latest Octopus version to $SHA."
   fi
 fi
+
+go build ./... && go install -v ./... && go test ./...
 
 echo ""
 echo "Suggestions from go-vet..."
