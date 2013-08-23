47c23f289f4c9e1762d825bb01961ec333341ba4
Jake Brukhman <brukhman@gmail.com>
2013-08-23 00:44:58 -0400

    Tests.

--- device/repository_test.go
+++ device/repository_test.go
@@ -5,10 +5,11 @@ package device
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
-const testBaseDir = "../var/poop"
+const testBaseDir = "../var/unit-tests/test-repo"
 
 // Test repository, basedir, and subdir
 // creation.
@@ -46,5 +47,32 @@ func TestNewRepository(t *testing.T) {
 		if _, err := os.Stat(subdir); os.IsNotExist(err) {
 			t.Errorf("did not create subdir")
 		}
+	}
+}
+
+func TestNewResourceId(t *testing.T) {
+	r, err := NewRepository(testBaseDir)
+
+	// something wrong with constructor
+	if err != nil {
+		t.Errorf("could not create the directories")
+	}
+
+	// test a local id
+	id, fp := r.NewResourceIdWithSubdir(SubdirLocal)
+	if id == "" || filepath.Dir(fp) != filepath.Join(r.basedir, SubdirLocal) || filepath.Base(fp) != id {
+		t.Errorf("something went wrong with id generation")
+	}
+
+	// test cloud id
+	id, fp = r.NewResourceIdWithSubdir(SubdirCloud)
+	if id == "" || filepath.Dir(fp) != filepath.Join(r.basedir, SubdirCloud) || filepath.Base(fp) != id {
+		t.Errorf("something went wrong with id generation")
+	}
+
+	// test a local id
+	id, fp = r.NewResourceId()
+	if id == "" || filepath.Dir(fp) != filepath.Join(r.basedir, SubdirLocal) || filepath.Base(fp) != id {
+		t.Errorf("something went wrong with id generation")
 	}
 }
