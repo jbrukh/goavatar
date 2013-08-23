b030ac80ed6cf428eb54635fb63080eae14b4bc9
Jake Brukhman <brukhman@gmail.com>
2013-08-23 00:39:26 -0400

    Added resourceId generation.

--- device/repository.go
+++ device/repository.go
@@ -4,9 +4,31 @@
 package device
 
 import (
+	. "github.com/jbrukh/goavatar/util"
 	"os"
 	"path/filepath"
 )
+
+// ----------------------------------------------------------------- //
+// Constants
+// ----------------------------------------------------------------- //
+
+// subdirectories
+const (
+	SubdirLocal = "local"
+	SubdirCloud = "cloud"
+)
+
+// max number of times to try to
+// generate a resource id on clash
+const maxGenerateRetries = 10
+
+// subdirectory search path
+var subdirSearchPath = []string{SubdirLocal, SubdirCloud}
+
+// ----------------------------------------------------------------- //
+// Repository
+// ----------------------------------------------------------------- //
 
 // A Repository encasulates an Octopus data repository. It is
 // local to a user's computer and performs all the operations
@@ -42,7 +64,7 @@ func NewRepository(basedir string) (r *Repository, err error) {
 	// create all the subdirs
 	for _, subdir := range subdirSearchPath {
 		fullPath := filepath.Join(basedir, subdir)
-		if err = os.Mkdir(fullPath, 0755); err != nil {
+		if err = os.MkdirAll(fullPath, 0755); err != nil {
 			return nil, err
 		}
 	}
@@ -50,15 +72,6 @@ func NewRepository(basedir string) (r *Repository, err error) {
 	// ok!
 	return
 }
-
-// subdirectories
-const (
-	SubdirLocal = "local"
-	SubdirCloud = "cloud"
-)
-
-// subdirectory search path
-var subdirSearchPath = []string{SubdirLocal, SubdirCloud}
 
 // Return the base directory of the repository.
 func (r *Repository) Basedir() string {
@@ -75,5 +88,25 @@ func (r *Repository) Subdirs() (subdirs []string) {
 }
 
 func (r *Repository) NewResourceId() (resourceId, resourcePath string) {
-	return
+	return r.NewResourceIdWithSubdir(SubdirLocal)
+}
+
+func (r *Repository) NewResourceIdWithSubdir(subdir string) (resourceId, resourcePath string) {
+	var fp, id string
+	// create a resource id
+	for i := 1; i <= maxGenerateRetries; i++ {
+		// try to generate the id
+		id, _ = Uuid()
+		fp = filepath.Join(r.basedir, subdir, id)
+
+		// check for clash
+		_, err := os.Stat(fp)
+		if err == nil && i == maxGenerateRetries {
+			// could not resolve clash
+			panic("could not generate a unique resourceId, nothing to be done")
+		}
+	}
+
+	// id is aquired, generate the path
+	return id, fp
 }
