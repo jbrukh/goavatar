03c94abb6845876725fcaf6eccfcb661a867dde5
Jake Brukhman <brukhman@gmail.com>
2013-08-29 00:44:25 -0400

    Handling SIGTERM gracefully.

--- cmd/octopus/octopus.go
+++ cmd/octopus/octopus.go
@@ -8,7 +8,21 @@ import (
 	. "github.com/jbrukh/goavatar/drivers"
 	. "github.com/jbrukh/goavatar/socket"
 	"log"
+	"os"
+	"os/signal"
 )
+
+var c = make(chan os.Signal, 1)
+
+func init() {
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			log.Printf("Terminating the Octopus Server (SIGTERM)...\n")
+			os.Exit(0)
+		}
+	}()
+}
 
 func main() {
 	flag.Parse()
