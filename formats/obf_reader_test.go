1cbf55b2073959da16a7444f225d5a7877d66b3a
Jake Brukhman <brukhman@gmail.com>
2013-09-02 16:34:50 -0400

    Unit test.

--- formats/obf_reader_test.go
+++ formats/obf_reader_test.go
@@ -17,7 +17,7 @@ func obfData(file string) (io.Reader, error) {
 	return os.Open(file)
 }
 
-func TestNewObfReader(t *testing.T) {
+func TestObfReader__New(t *testing.T) {
 	r, err := obfData(testFile1)
 	if err != nil {
 		t.Errorf("could not get mock data: %v", err)
@@ -28,4 +28,63 @@ func TestNewObfReader(t *testing.T) {
 		t.Errorf("could not init reader: %v", err)
 	}
 
+	h := re.Header()
+	channels, samples := h.Dim()
+	if channels != 2 || samples != 3024 {
+		t.Errorf("unexpected dimensions")
+	}
+
+	if h.DataType != 1 || h.FormatVersion != 2 || h.StorageMode != 1 ||
+		h.SampleRate != 250 || h.Endianness != 0 || h.IndexUnit != 0 {
+		t.Errorf("unexpected header")
+	}
+}
+
+func TestObfReader__Parallel(t *testing.T) {
+	r, err := obfData(testFile1)
+	if err != nil {
+		t.Errorf("could not get mock data: %v", err)
+	}
+
+	re, err := NewObfReader(r)
+	if err != nil {
+		t.Errorf("could not init reader: %v", err)
+	}
+
+	b, err := re.Parallel()
+	if err != nil {
+		t.Errorf("could not deserialize: %v", err)
+	}
+
+	if b.Channels() != 2 || b.Samples() != 3024 {
+		t.Errorf("unexpected dimensions")
+	}
+
+	v, inx := b.Sample(0)
+	if inx != 0 || len(v) != 2 {
+		t.Errorf("unexpected first sample")
+	}
+
+	v, inx = b.Sample(1)
+	if inx != 4000000 || len(v) != 2 {
+		t.Errorf("unexpected second sample")
+	}
+}
+
+func TestObfReader__Sequential(t *testing.T) {
+	r, err := obfData(testFile2)
+	if err != nil {
+		t.Errorf("could not get mock data: %v", err)
+	}
+
+	re, err := NewObfReader(r)
+	if err != nil {
+		t.Errorf("could not init reader: %v", err)
+	}
+
+	h := re.Header()
+	if h.Channels != 1 || h.Samples != 10706 || h.DataType != 1 || h.FormatVersion != 2 || h.StorageMode != 3 ||
+		h.SampleRate != 512 || h.Endianness != 0 || h.IndexUnit != 0 {
+		t.Errorf("unexpected header")
+	}
 }
